d9bfc60fc884858bbb9ae013d7efda4a32fc61a5
alshanewi <alshanewi@gmail.com>
2015-03-19 23:38:44 +0100

    refactored package presentation

---
+++ src/de/tests/HandTest.java
@@ -0,0 +1,115 @@
+package de.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.pokergame.Hand;
+
+public class HandTest {
+	private Hand hand;
+
+	@Before
+	public void executedBeforeEach() {
+		hand = new Hand();
+	}
+
+	@Test
+	public void testIsStraight() {
+		String cardString = "2H 3D 4S 5C 6D";
+		hand.drawFromDeck(cardString);
+		assertTrue(hand.isStraight());
+	}
+
+	@Test
+	public void testIsStraightDiffernetSortingFromTheBeginning() {
+		String cardString = "2H 3D 6S 5C 4D";
+		hand.drawFromDeck(cardString);
+		assertTrue(hand.isStraight());
+	}
+
+	@Test
+	public void testIsNotStraight() {
+		String cardString = "2H 3D 4S 5C 5D";
+		hand.drawFromDeck(cardString);
+		assertFalse(hand.isStraight());
+	}
+
+	@Test
+	public void testIsFlush() {
+		String cardString = "2H 6H 3H 5H 6H";
+		hand.drawFromDeck(cardString);
+		assertTrue(hand.isFlush());
+	}
+
+	@Test
+	public void testIsStraightFlush() {
+		String cardString = "2H 3H 4H 5H 6H";
+		hand.drawFromDeck(cardString);
+		assertTrue(hand.isStraightFlush());
+	}
+
+	@Test
+	public void testIsFullHouse() {
+		String cardString = "9H 9H 9H 2H 2H";
+		hand.drawFromDeck(cardString);
+		assertTrue(hand.isFullHouse());
+	}
+
+	@Test
+	public void testIsNotFullHouse() {
+		String cardString = "9H 9H 9H 3H 2H";
+		hand.drawFromDeck(cardString);
+		assertFalse(hand.isFullHouse());
+	}
+
+	@Test
+	public void testIsFourOfKind() {
+		String cardString = "9C 9H 9D 9S 2H";
+		hand.drawFromDeck(cardString);
+		assertTrue(hand.isFourOfKind());
+	}
+
+	@Test
+	public void testIsThreeOfKind() {
+		String cardString = "9C 9H 9D 8S 8H";
+		hand.drawFromDeck(cardString);
+		assertTrue(hand.isThreeOfKind());
+	}
+
+	@Test
+	public void testIsTwoPairs() {
+		String cardString = "9C 9H 8D 8S 7H";
+		hand.drawFromDeck(cardString);
+		assertTrue(hand.isTwoPairs());
+	}
+
+	@Test
+	public void testIsNotTwoPairs() {
+		String cardString = "9C 9H 8D 8S 8H";
+		hand.drawFromDeck(cardString);
+		assertFalse(hand.isTwoPairs());
+	}
+
+	@Test
+	public void testIsPair() {
+		String cardString = "9C 9H 6D 5S 8H";
+		hand.drawFromDeck(cardString);
+		assertTrue(hand.isPair());
+	}
+	
+	@Test
+	public void testIsPair1() {
+		String cardString = "5H 5C 6S 7S KD";
+		hand.drawFromDeck(cardString);
+		assertTrue(hand.isPair());
+	}
+
+	@Test
+	public void testIsNotPair() {
+		String cardString = "9C 9H 6D 6S 8H";
+		hand.drawFromDeck(cardString);
+		assertFalse(hand.isPair());
+	}
+}
---
+++ src/de/tests/PokerTableTest.java
@@ -0,0 +1,46 @@
+package de.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import de.pokergame.PokerTable;
+
+public class PokerTableTest {
+
+	private String cardsInputLine1 = "5H 5C 6S 7S KD 2C 3S 8S 8D TD";
+	private String cardsInputLine2 = "5D 8C 9S JS AC 2C 5C 7D 8S QH";
+	private String cardsInputLine3 = "2D 9C AS AH AC 3D 6D 7D TD QD";
+	private String cardsInputLine4 = "4D 6S 9H QH QC 3D 6D 7H QD QS";
+	private String cardsInputLine5 = "2H 2D 4C 4D 4S 3C 3D 3S 9S 9D";
+
+	@Test
+	public void testPairOfEightWins() {
+		assertTrue(PokerTable.SECOND_HAND_WINS.equals(PokerTable
+				.pokerHands(cardsInputLine1)));
+	}
+
+	@Test
+	public void testHighestCardAceWins() {
+		assertTrue(PokerTable.FIRST_HAND_WINS.equals(PokerTable
+				.pokerHands(cardsInputLine2)));
+	}
+
+	@Test
+	public void testFlushWithDiamondsWins() {
+		assertTrue(PokerTable.SECOND_HAND_WINS.equals(PokerTable
+				.pokerHands(cardsInputLine3)));
+	}
+
+	@Test
+	public void testPairOfQueensHighestCardNineWins() {
+		assertTrue(PokerTable.FIRST_HAND_WINS.equals(PokerTable
+				.pokerHands(cardsInputLine4)));
+	}
+
+	@Test
+	public void testFullHouseWithThreeFoursWins() {
+		assertTrue(PokerTable.FIRST_HAND_WINS.equals(PokerTable
+				.pokerHands(cardsInputLine5)));
+	}
+}
